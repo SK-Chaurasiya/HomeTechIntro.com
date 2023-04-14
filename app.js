89baf23863754b801df1b210e9c0d3a857e0b3ba
White-Hat-Designer <97239651+SK-Chaurasiya@users.noreply.github.com>
2023-04-14 21:09:32 +0530

    New file create
    App.js file is add

---
+++ app.js
@@ -0,0 +1,62 @@
+let animation = anime({
+  targets: 'header',
+  translateY: [-40, 0],
+  opacity: [0, 1],
+  duration: 1000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.title',
+  translateX: [-80, 0],
+  opacity: [0, 1],
+  duration: 2000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.text',
+  translateX: [80, 0],
+  opacity: [0, 1],
+  duration: 2000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.content-btn',
+  translateY: [80, 0],
+  opacity: [0, 1],
+  duration: 2000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.comouter',
+  translateX: [80, 0],
+  opacity: [0, 1],
+  duration: 3000,
+  easing: 'easeInOutExpo',
+});
+anime({
+  targets: '.playbtn',
+  translateY: [80, 0],
+  opacity: [0, 1],
+  duration: 3000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.mouse',
+  translateX: [-80, 0],
+  opacity: [0, 1],
+  duration: 3000,
+  easing: 'easeInOutExpo',
+});
+
+anime({
+  targets: '.conar-circle',
+  translateX: [-80, 0],
+  opacity: [0, 1],
+  duration: 3000,
+  easing: 'easeInOutExpo',
+});
